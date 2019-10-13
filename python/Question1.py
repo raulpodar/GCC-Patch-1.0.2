82ae59c44e57682fa0d766fdd416ca9ab87cf844
raulpodar <33556510+raulpodar@users.noreply.github.com>
2019-10-13 16:37:20 +0100

    solution1

--- python/Question1.py
+++ python/Question1.py
@@ -1,5 +1,15 @@
 # modify this function, and create other functions below as you wish
 def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
-    # modify and then return the variable below
-    answer = -5
+    deposit=0.1*initialLevelOfDebt
+    answer=deposit
+    initialDebt=initialLevelOfDebt
+    count=0
+    while(initialLevelOfDebt>=50):
+        if(initialLevelOfDebt<50) :monthlyRepayment=initialLevelOfDebt
+        else: monthlyRepayment=initialDebt*(repaymentPercentage/100)
+        count+=1
+        debtAfterinterestPercentage=initialLevelOfDebt*(interestPercentage/100)+initialLevelOfDebt
+        initialLevelOfDebt=debtAfterinterestPercentage-monthlyRepayment
+        answer+=monthlyRepayment
+    answer=initialLevelOfDebt+answer
     return answer
